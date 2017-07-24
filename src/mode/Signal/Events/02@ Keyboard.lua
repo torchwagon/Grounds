5c2bdf98ddfc33f1b42d6fc5b818624fdf2bf4c6
Lautenschlager <tai.romani@hotmail.com>
2017-07-24 18:39:20 -0300

    Delete 02@ Keyboard.lua

--- src/mode/Signal/Events/02@ Keyboard.lua
+++
@@ -1,8 +0,0 @@
-	eventKeyboard = function(n,k,d)
-		if mode.signal.sys[2] == 3 and d and os.time() > mode.signal.discrepancy then
-			tfm.exec.killPlayer(n)
-		else
-			mode.signal.info[n].isMoving[k + 1] = d
-		end
-		mode.signal.info[n].afk = false
-	end,
