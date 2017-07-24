064ab13eb514a99653dc13d76e07ccdd14974b13
Lautenschlager <tai.romani@hotmail.com>
2017-07-24 18:35:43 -0300

    Add files via upload

--- src/mode/Hardcamp/Events/02@ NewPlayer.lua
+++ src/mode/Hardcamp/Events/02@ NewPlayer.lua
@@ -17,8 +17,5 @@
 		system.bindKeyboard(n,46,true,true) -- Delete key
 		tfm.exec.chatMessage("<T>" .. system.getTranslation("welcome") .. "\n\t<CEP>/w Mquk #bootcamp+ @mapCode",n)
 		
-		local id = tfm.exec.addImage("15c6ee6324d.png","&0",120,100,n)
-		system.newTimer(function()
-			tfm.exec.removeImage(id)
-		end,5000,false)
+		ui.banner("15d75ac6aa9",120,100,n)
 	end,
